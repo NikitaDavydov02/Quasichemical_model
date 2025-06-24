96f3a6152401e76c0589001fb0ee8d63d9498120
Nikita Davydov <nikita.davidov02@yandex.ru>
2025-06-24 19:46:05 +0300

    Enhanced method for p_matrix calculation is added

--- scheutjens_model.cpp
+++ scheutjens_model.cpp
@@ -4,11 +4,11 @@
 
 void Scheutjens_Model::Init() {
     // Initialization logic here
-    z=10;       
-    r=10;       // Chain length
-    M=3*r;      // Number of layers
-    chi = 1.2;  // Flory parameter
-    chi_s=0.0;  // Adsorption parameter
+      
+    r=1000;       // Chain length
+    M=20;      // Number of layers
+    chi = 0.0;  // Flory parameter
+    chi_s= 0.0;  // Adsorption parameter
     phi_bulk[0]=0.01;
     phi_bulk[1]=1-phi_bulk[0];
 
@@ -22,8 +22,11 @@ void Scheutjens_Model::Init() {
     avr_phi_i.setZero(M);
     phi_i_dashed.setZero(M);
     
-    double lamda_0 = 4.0/6.0;
-    double lamda_1 = 1.0/6.0;
+    //lamda_0 = 4.0/6.0;
+    //lamda_1 = 1.0/6.0;
+
+    lamda_0=0.5;
+    lamda_1=0.25;
 
     // Initialize the lambda matrix with some values, for example:
     for(int i=0; i<M; i++) {
@@ -39,7 +42,7 @@ void Scheutjens_Model::Init() {
     std::cout << "Lambda matrix initialized:" << std::endl;
     Print_Matrix(lamda_matrix);
 
-    lnP_bulk =chi*(phi_bulk[0]-phi_bulk[1])+log(phi_bulk[1]);//[TO DO] check if this is correct
+    lnP_bulk =chi*(phi_bulk[0]-phi_bulk[1])+log(phi_bulk[0]);//[TO DO] check if this is correct
 
     initialized = true; // Set initialized to true after initialization
 }
@@ -63,11 +66,27 @@ void Scheutjens_Model::function( Eigen::VectorXd& xs,  Eigen::VectorXd& ys, int 
     Print_Vector(avr_phi_solv_i);
 
 
-    //<calculate lnPi>
+    Calculate_ln_p_i();
+    Print_Matrix(p_matrix);
+
+    Calculate_p_matrix();
+
+    Calculate_phi_i_dashed();
+
+    //<calculate ys>
+    for(int i=0; i<M; i++)
+    {
+        ys(i) = log(phi_i_dashed(i) / phi_i(i)); // Calculate ys
+    }
+    Print_Vector(ys);
+    //</calculate ys>
+}
+void Scheutjens_Model::Calculate_ln_p_i()
+{
     for(int i=0; i<M; i++)
     {
        // double lnP_i = chi*(avr_phi_i[i]-avr_phi_solv_i[i])+log(avr_phi_solv_i[i]);//[TO DO] check if this is correct
-        double lnP_i = chi*(avr_phi_i[i]-avr_phi_solv_i[i])+log(avr_phi_solv_i[i]);
+        double lnP_i = chi*(avr_phi_i[i]-avr_phi_solv_i[i])+log(avr_phi_i[i]);
 
         if(i==0)
             lnP_i += chi_s;
@@ -80,22 +99,9 @@ void Scheutjens_Model::function( Eigen::VectorXd& xs,  Eigen::VectorXd& ys, int 
             w_matrix(i, j) = lamda_matrix(i, j) * p_i; // Calculate w_matrix
         }
     }
-    Print_Matrix(p_matrix);
-    //</calculate lnPi>
-
-    //<calculate p_matrix>
-    Eigen::VectorXd p_col_prev = p_matrix.col(0);
-    Eigen::VectorXd p_col_new;
-    for(int i=1; i<r; i++)
-    {
-        p_col_new = w_matrix * p_col_prev; // Calculate new column of p_matrix
-        p_matrix.col(i) = p_col_new; // Store in p_matrix
-        p_col_prev = p_col_new; // Update for next iteration
-    }
-    Print_Matrix(p_matrix);
-    //</calcualte p_matrix>
-
-    //<calculate phi_i_dashed>
+}
+void Scheutjens_Model::Calculate_phi_i_dashed()
+{
     for(int i=0;i<M;i++)
     {
         double sum=0;
@@ -109,15 +115,65 @@ void Scheutjens_Model::function( Eigen::VectorXd& xs,  Eigen::VectorXd& ys, int 
         phi_i_dashed(i)=phi_bulk[0]*sum/(r*p_matrix(i,0)); // Calculate phi_i_dashed
     }
     Print_Vector(phi_i_dashed);
-    //</calculate phi_i_dashed>
-
-    //<calculate ys>
-    for(int i=0; i<M; i++)
+}
+void Scheutjens_Model::Calculate_p_matrix()
+{
+    /*<OLD>Eigen::VectorXd p_col_prev = p_matrix.col(0);
+    Eigen::VectorXd p_col_new;
+    for(int i=1; i<r; i++)
     {
-        ys(i) = log(phi_i_dashed(i) / phi_i(i)); // Calculate ys
+        p_col_new = w_matrix * p_col_prev; // Calculate new column of p_matrix
+        p_matrix.col(i) = p_col_new; // Store in p_matrix
+        p_col_prev = p_col_new; // Update for next iteration
     }
-    Print_Vector(ys);
-    //</calculate ys>
+    Print_Matrix(p_matrix);
+    </OLD>*/
+
+    //New implementation
+    Eigen::VectorXd p_col_prev = p_matrix.col(0);
+    Eigen::VectorXd p_col_new;
+    int last_ascent_column_index = 0;
+    if(r%2==0)
+        last_ascent_column_index = r/2; // Last column index for ascending part
+    else
+        last_ascent_column_index = (r+1)/2; // Last column index for ascending part
+
+    for(int s=1;s<r;s++)
+    {
+        if(s<=last_ascent_column_index)
+        {
+            int new_col_size = M+s; // Size of the new column vector
+            p_col_new.setZero(new_col_size);
+            for(int i=0;i<new_col_size;i++)
+            {
+                if(i==0)
+                    p_col_new(i)=lamda_0*p_col_prev(i)+lamda_1*p_col_prev(i+1); // First layer
+                else if(i==new_col_size-1)
+                    p_col_new(i)=lamda_1*p_col_prev(i-1)+lamda_0*1 + lamda_1*1;
+                else if(i==new_col_size-2)
+                    p_col_new(i)=lamda_1*p_col_prev(i-1)+lamda_0*p_col_prev(i) + lamda_1*1;
+                else
+                    p_col_new(i)=lamda_1*p_col_prev(i-1)+lamda_0*p_col_prev(i)+lamda_1*p_col_prev(i+1);
+            }
+            p_matrix.col(s)=p_col_new.segment(0, M); // Store in p_matrix
+            p_col_prev= p_col_new;
+        }
+        else
+        {
+            int distance_till_end = r-1-s; // Distance to the end of a trigangle
+            int new_col_size = M+distance_till_end; // Size of the new column vector
+            p_col_new.setZero(new_col_size);
+            for(int i=0;i<new_col_size;i++)
+            {
+                if(i==0)
+                    p_col_new(i)=lamda_0*p_col_prev(i)+lamda_1*p_col_prev(i+1); // First layer
+                else
+                    p_col_new(i)=lamda_1*p_col_prev(i-1)+lamda_0*p_col_prev(i)+lamda_1*p_col_prev(i+1);
+            }
+            p_matrix.col(s)=p_col_new.segment(0, M); // Store in p_matrix
+            p_col_prev = p_col_new;
+        }
+    }
 }
 void Scheutjens_Model::Calculate_avr_phi_i()
 {
@@ -125,13 +181,13 @@ void Scheutjens_Model::Calculate_avr_phi_i()
     avr_phi_solv_i = lamda_matrix * phi_solv_i;
 }
 void Scheutjens_Model::Print_Vector(const Eigen::VectorXd& vec) {
-    //return; // Commented out to avoid excessive output
+    return; // Commented out to avoid excessive output
     std::cout << std::endl;
     std::cout << std::endl;
     std::cout << vec << std::endl;
 }
 void Scheutjens_Model::Print_Matrix(const Eigen::MatrixXd& mat) {
-   // return; // Commented out to avoid excessive output
+    return; // Commented out to avoid excessive output
     std::cout << std::endl;
     std::cout << std::endl;
     std::cout << mat << std::endl;
--- scheutjens_model.h
+++ scheutjens_model.h
@@ -8,10 +8,12 @@ class Scheutjens_Model{
     void Calculate_avr_phi_i();
     void Print_Vector(const Eigen::VectorXd& vec);
     void Print_Matrix(const Eigen::MatrixXd& mat);
+    void Calculate_p_matrix();
+    void Calculate_phi_i_dashed();
+    void Calculate_ln_p_i();
 
     bool initialized;
     int M;
-    int z;
     int r;
     double chi;   //Flory parameter
     double chi_s; //Adsorption_parameter
@@ -20,6 +22,8 @@ class Scheutjens_Model{
     Eigen::VectorXd phi_solv_i;
     Eigen::VectorXd phi_i_dashed;
     double lnP_bulk;
+    double lamda_0;
+    double lamda_1;
 
     Eigen::VectorXd  avr_phi_i; //average molar fraction of polymer in each layer
     Eigen::VectorXd  avr_phi_solv_i; //average molar fraction of solvent in each layer
